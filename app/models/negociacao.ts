ed75b63879616c0587413b5d5c727019d5f6ff11
Rodrigo Costa <rodrigocosta47@outlook.com>
2022-03-18 16:01:36 -0300

    Finalzei a primeira etapa do curso

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,28 +1,18 @@
 class Negocioacao {
-    private _data: Date;
-    private _quantidade: number;
-    private _valor: number;
 
-    constructor(data: Date, quantidade: number, valor: number) {
-        this._data = data;
-        this._quantidade = quantidade;
-        this._valor = valor;
-    }
-
-    get data(): Date {
-        return this._data;
-    }
-
-    get quantidade(): number {
-        return this._quantidade
-    }
-
-    get valor(): number {
-        return this._valor
+    constructor(
+        public _data: Date,
+        public readonly quantidade: number,
+        public readonly valor: number
+        ) {}
+    
+    get data(): Date{
+        const data = new Date(this._data.getTime());
+        return data;
     }
 
     get volume(): number {
-        return this._valor * this._quantidade
+        return this.valor * this.quantidade
     }
 }
 
